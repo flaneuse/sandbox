1e231c45019058c17996daea808a3b48c6f9853c
flaneuse <laura.d.hughes@gmail.com>
2017-01-24 16:57:15 -0500

    v3 d3_rebind

---
+++ scrolly/js/d3_rebind.js
@@ -0,0 +1,18 @@
+// https://github.com/d3/d3/blob/v3.5.17/src/core/rebind.js
+
+// Copies a variable number of methods from source to target.
+d3.rebind = function(target, source) {
+  var i = 1, n = arguments.length, method;
+  while (++i < n) target[method = arguments[i]] = d3_rebind(target, source, source[method]);
+  return target;
+};
+
+// Method is assumed to be a standard D3 getter-setter:
+// If passed with no arguments, gets the value.
+// If passed with arguments, sets the value and returns the target.
+function d3_rebind(target, source, method) {
+  return function() {
+    var value = method.apply(source, arguments);
+    return value === source ? target : value;
+  };
+}
